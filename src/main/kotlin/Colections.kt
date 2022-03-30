733d140bad9887a909294636d1280be09e7c2bd8
Carlos Camponuevo <carlos@ipremios.com>
2022-03-30 10:02:15 -0300

    Add Colections

---
+++ src/main/kotlin/Colections.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+// ArrayList - Classe
+val frutas1 = ArrayList<String>()
+frutas1.add("Maça")
+frutas1.add("Banana")
+frutas1.add("Morango")
+println(frutas1)
+
+// ArrayList - função
+val frutas2 = arrayListOf("Maça", "Banana", "Morango")
+
+// Verificar se é da Biblioteca Padrão Java
+println(HashSet::class.java)
+
+// Kotlin proveitos
+    println(frutas2.last())
+
+}
